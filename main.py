de8ea780a5f727d23be64d45c2066025a7eace35
Gustavo Jesus <gustavo>
2024-11-07 11:47:46 +0100

    exercice 4 nombres premiers

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+"""Module math qui utilise sqrt pour calculer la racine carrée d'un nombre."""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """Fonction qui renvoit si un nombre est premier ou non"""
 
+    if p in (0, 1):
+        return False
     for i in range(2, int(sqrt(p)) + 1):
         if p % i == 0:
             return False
-    else:   
-        return True
+
+    return True
 
 #### Fonction principale
 
-
 def main():
-
+    """Fonction qui affiche tous les nombres premiers entre 0 et 100"""
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
